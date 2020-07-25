5633ad179a272060fdfd84a4f9916d0c5f744815
Jeswin <jeswinpk@agilehead.com>
2020-07-25 11:11:01 +0530

    Update count-word-occurences.sh

--- count-word-occurences.sh
+++ count-word-occurences.sh
@@ -2,4 +2,4 @@
 
 # The first argument is the text to match
 # echo "some lalala long text goes here lalala" | ./count-word-occurences.sh lalala
-basho -d word "\"$1\"" -j '(x.match(new RegExp(k.word, "g")) || []).length'
+basho -d word "\"$1\"" -r 'acc+x' '""' -j '(x.match(new RegExp(k.word, "g")) || []).length'
