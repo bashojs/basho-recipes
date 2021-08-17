0f7eca83f7d675becb90f953423aa1d9602a004b
Jeswin <jeswinpk@agilehead.com>
2021-08-17 10:49:39 +0530

    Update and rename git-repos-to-commit-or-push.sh to repos-to-push.sh

--- git/repos-to-push.sh
+++ git/repos-to-push.sh
@@ -1,18 +1,20 @@
 # Usage
-# ./git-repos-to-commit-or-push.sh
+# ./repos-to-push.sh
 
 # Explanation
 #
 # 1. Pipe the output of ls to basho
-# 2. flatten the array to get a list of directory names
+# 2. Directory entries start with 'd'. Filter them.
 # 3. Named each item 'dirname'
 # 4. cd into the directory and do a 'git status'
 # 5. Check if the output of 'git status' has 'nothing to commit' or 'branch is up-to-date'
 # 6. If not, print the previously saved 'dirname'
 
-ls -alt | basho 'x.split(/\s+/)' \
-  -f 'x.length>2' \
-  -j 'x.slice(-1)[0]' \
+ls -alt | \
+  basho 'x.split(/\s+/)'  \
+  -f 'x.length>2'  \
+  -f 'x[0].startsWith("d")' \
+  -j 'x.slice(-1)[0]'  \
   -f '![".", ".."].includes(x)' \
   -n dirname \
   -e 'cd ${x} && git status' -f 'x.some(_ => /branch is ahead/.test(_)) || (!x.some(_ => /nothing to commit/.test(_)) && !x.some(_ => /branch is up-to-date/.test(_)))' \
